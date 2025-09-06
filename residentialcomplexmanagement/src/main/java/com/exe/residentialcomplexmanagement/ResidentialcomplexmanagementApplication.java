6d89449ed9e2d96d4c24afa0f43b464ce64565f1
YEFERSON DE JESUS SANCHEZ VEGA <jefersonsnachez@gmail.com>
2025-09-06 09:52:49 -0500

    Add files via upload

---
+++ residentialcomplexmanagement/src/main/java/com/exe/residentialcomplexmanagement/ResidentialcomplexmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.exe.residentialcomplexmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentialcomplexmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidentialcomplexmanagementApplication.class, args);
+	}
+
+}
---
+++ residentialcomplexmanagement/src/test/java/com/exe/residentialcomplexmanagement/ResidentialcomplexmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.residentialcomplexmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentialcomplexmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
